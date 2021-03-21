e25fc7fee15d94169a2512cb8e82226fb17ac907
raydiana <69117317+raydiana@users.noreply.github.com>
2021-03-21 17:05:20 +0100

    Update gettext.py

--- gettext.py
+++ gettext.py
@@ -169,38 +169,6 @@ def graph(nodes):
         f.write(s)
 
 
-"""
-def sentence_generator():
-    re_filename = re.compile("^\w{3}(?:.txt)$")
-    for file in os.listdir(): #Pour chaque fichier
-        if re_filename.match(file): # Si le fichier est en .txt
-            with open(file, 'r') as f:
-                robot=f.read() 
-                keyword=robot.split()
-
-                lists=[]
-                for i in range(len(keyword)-1): # Pour chaque ligne
-                    lists.append((keyword[i], keyword[i+1]))
-
-                dictionnary={}
-                for begin, end in lists:
-                    if begin in dictionnary.keys():
-                        dictionnary[begin].append(end)
-                    else:
-                        dictionnary[begin]=[end]
-
-                first_word=np.random.choice(keyword)
-                while first_word.islower():
-                    first_word=np.random.choice(keyword)
-                    chain=[first_word]
-                    count=100
-                    for i in range(count):
-                        chain.append(np.random.choice(dictionnary[\
-                            chain[-1]]))
-                        sentences=' '.join(chain)
-                    print(sentences)
-                    
-"""
                     
 def sentence_generator():
     #nltk.download('punkt')
